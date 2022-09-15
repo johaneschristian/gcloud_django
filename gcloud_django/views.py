797a470bb88ed662dcab252605ccf38328a73f65
johanes.christian <johanes.christian@ui.ac.id>
2022-09-15 15:04:39 +0700

    ..

--- gcloud_django/views.py
+++ gcloud_django/views.py
@@ -2,4 +2,4 @@ from django.http.response import JsonResponse
 
 
 def hello_world(request):
-    return JsonResponse(data='Hello World, It\'s Me', safe=False)
+    return JsonResponse(data='Hell World, It\'s Me', safe=False)
