9d3d37dc88b63f32f4de92961519339d3c490fa0
johanes.christian <johanes.christian@ui.ac.id>
2022-09-17 10:50:31 +0700

    False sonarqube

--- gcloud_django/views.py
+++ gcloud_django/views.py
@@ -14,3 +14,12 @@ def get_cloud_run_url(request):
 def get_environ(request):
     environs = dict(os.environ)
     return JsonResponse(data=environs, safe=False)
+
+# def hello_world(request):
+#     return JsonResponse(data='Hella World, It\'s Me', safe=False)
+# def get_cloud_run_url(request):
+#     cloud_run_url = os.getenv('CLOUDRUN_SERVICE_URL', default='Not Found')
+#     return JsonResponse(data=cloud_run_url, safe=False)
+# def get_environ(request):
+#     environs = dict(os.environ)
+#     return JsonResponse(data=environs, safe=False)
